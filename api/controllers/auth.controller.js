28155f8a53089b39a0912afe32fde6f7f2014ed4
sandking-number1 <Alfred.J9412@gmail.com>
2024-05-10 01:25:15 +0530

    sign in pg

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res , next) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
 
     const hashedPassword = bcryptjs.hashSync(password, 10)
@@ -11,6 +12,24 @@ export const signup = async (req, res , next) => {
         await newUser.save()
         res.status(201).json("User  created successfully")
     } catch (error) {
-       next(error   )
+        next(error)
+    }
+}
+
+export const signiin = async (req, res, next) => {
+    const { email, password } = req.body
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if (!validPassword) {
+            return next(errorHandler(401, 'Wrong credentials'))
+        }
+    } catch (error) {
+        next(error)
     }
 }
