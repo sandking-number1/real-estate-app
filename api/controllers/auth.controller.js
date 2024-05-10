2b3004ac6df8a6b134bc451ca3e4896839b1831a
sandking-number1 <Alfred.J9412@gmail.com>
2024-05-10 09:55:39 +0530

    sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -16,7 +17,7 @@ export const signup = async (req, res, next) => {
     }
 }
 
-export const signiin = async (req, res, next) => {
+export const signin = async (req, res, next) => {
     const { email, password } = req.body
     try {
         const validUser = await User.findOne({ email })
@@ -29,6 +30,16 @@ export const signiin = async (req, res, next) => {
         if (!validPassword) {
             return next(errorHandler(401, 'Wrong credentials'))
         }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        // not sending the pass back to user as json
+        const {password : pas , ...rest} = validUser._doc
+
+        res.cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest)
+
     } catch (error) {
         next(error)
     }
--- api/index.js
+++ api/index.js
@@ -16,6 +16,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+
+//error middleware
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
