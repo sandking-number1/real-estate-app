ae372bbf58f5c2047fa28eb966d5d5f107b9d466
sandking-number1 <Alfred.J9412@gmail.com>
2024-05-12 18:42:29 +0530

    listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 dotenv.config();
 const app = express();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/listing", listingRouter)
 
 //error middleware
 app.use((err, req, res, next) => {
---
+++ api/routes/listing.route.js
@@ -0,0 +1,10 @@
+import express, { Router } from "express";
+const router = express.Router()
+
+import { createListing } from "../controllers/listing.controller.js";
+
+import { verifyToken } from "../utils/verifyUser.js";
+
+router.post('/create', verifyToken, createListing)
+
+export default router
